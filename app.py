1c635d6bc42f3e341a54a149c8afe547db82d922
Ali Ahmed <ali.ahmed@uk.bmt.org>
2024-01-04 13:19:12 +0000

    init comit

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
-import requests
-from flask import Flask, jsonify
-import os
+print('Hello world')
+# import requests
+# from flask import Flask, jsonify
+# import os
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/rick-morty-api')
-def get_azure_sql_data():
-    # Replace this with the actual URL of your API endpoint
-    api_endpoint = 'https://rickandmortyapi.com/api/character/1'
+# @app.route('/rick-morty-api')
+# def get_azure_sql_data():
+#     # Replace this with the actual URL of your API endpoint
+#     api_endpoint = 'https://rickandmortyapi.com/api/character/1'
 
-    try:
-        # Make a request to the API endpoint with SSL verification disabled
-        api_response = requests.get(api_endpoint, verify=False)
+#     try:
+#         # Make a request to the API endpoint with SSL verification disabled
+#         api_response = requests.get(api_endpoint, verify=False)
 
-        # Raise an exception for bad status codes
-        api_response.raise_for_status()
+#         # Raise an exception for bad status codes
+#         api_response.raise_for_status()
 
-        # Get the JSON response
-        data = api_response.json()
+#         # Get the JSON response
+#         data = api_response.json()
 
-        # Process the data as needed
+#         # Process the data as needed
 
-        # Return a JSON response
-        return jsonify(data)
-    except requests.exceptions.RequestException as e:
-        # Handle errors
-        return jsonify({'error': str(e)}), 500
+#         # Return a JSON response
+#         return jsonify(data)
+#     except requests.exceptions.RequestException as e:
+#         # Handle errors
+#         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
